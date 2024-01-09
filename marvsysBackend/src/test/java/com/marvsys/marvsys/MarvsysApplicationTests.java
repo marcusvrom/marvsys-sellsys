a9ea819c536b0dbee569644c07175e39a7fc73d2
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2024-01-09 14:20:44 -0300

    [ADD]: projeto criado

---
+++ marvsysBackend/src/test/java/com/marvsys/marvsys/MarvsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marvsys.marvsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
